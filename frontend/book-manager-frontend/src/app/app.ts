f111aec66acc4c92c6ca06824d114af3ca010537
IagoAntunes <iagoantunes.f@gmail.com>
2026-01-15 17:19:17 -0300

    feat: Button Component

--- frontend/book-manager-frontend/src/app/app.ts
+++ frontend/book-manager-frontend/src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CButton } from "./components/c-button/c-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('book-manager-frontend');
+
+  teste(){
+    console.log('clicou no botão');
+  }
+
 }
---
+++ frontend/book-manager-frontend/src/app/components/c-button/c-button.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+
+export type ButtonTheme = 'primary' | 'secondary' | 'danger';
+
+
+@Component({
+  selector: 'app-c-button',
+  imports: [CommonModule],
+  templateUrl: './c-button.html',
+  styleUrl: './c-button.scss',
+})
+export class CButton {
+  theme = input<ButtonTheme>('primary');
+  disabled = input<boolean>(false);
+  icon = input<string | undefined>();
+
+  clicked = output<void>();
+  handleClick(){
+    if(!this.disabled()){
+      this.clicked.emit();
+    }
+  }
+
+}
