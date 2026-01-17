b128a451b26af612e1ef90ebc525b99f4213be33
IagoAntunes <iagoantunes.f@gmail.com>
2026-01-17 09:28:57 -0300

    feat: Enhance form validation and error handling in login and field components

--- frontend/book-manager-frontend/src/app/components/c-field/c-field.ts
+++ frontend/book-manager-frontend/src/app/components/c-field/c-field.ts
@@ -1,17 +1,10 @@
-import { Component, input, forwardRef, signal } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { Component, input, forwardRef, signal, Self, Optional } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-c-field',
   standalone: true,
   imports: [ReactiveFormsModule],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => CField),
-      multi: true
-    }
-  ],
   templateUrl: './c-field.html',
   styleUrl: './c-field.scss',
 })
@@ -21,17 +14,31 @@ export class CField implements ControlValueAccessor {
   rows = input<number>(1);
 
   value = signal<string>('');
-  
-  onChange: any = () => {};
-  onTouched: any = () => {};
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  constructor(@Self() @Optional() public controlDir: NgControl) {
+    if (this.controlDir) {
+      this.controlDir.valueAccessor = this;
+    }
+  }
+
+  get hasError(): boolean {
+    return !!(
+      this.controlDir &&
+      this.controlDir.invalid &&
+      this.controlDir.touched
+    );
+  }
 
   handleInput(event: Event) {
     const val = (event.target as HTMLInputElement).value;
     this.value.set(val);
-    this.onChange(val); 
+    this.onChange(val);
   }
 
   writeValue(val: string): void { this.value.set(val); }
-  registerOnChange(fn: any): void { this.onChange = fn; } 
+  registerOnChange(fn: any): void { this.onChange = fn; }
   registerOnTouched(fn: any): void { this.onTouched = fn; }
 }
--- frontend/book-manager-frontend/src/app/pages/login/login.ts
+++ frontend/book-manager-frontend/src/app/pages/login/login.ts
@@ -1,7 +1,7 @@
-import { Component, inject, Signal, signal } from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
 import { CField } from "../../components/c-field/c-field";
 import { CButton } from "../../components/c-button/c-button";
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 export type AuthType = 'login' | 'register';
 
@@ -22,27 +22,41 @@ export class Login {
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  handleAuthClick(){
-    if(this.authType() === 'login'){
-      this._onLoginClick();
+  title = computed(() => this.authType() === 'login' ? 'Acesse a plataforma' : 'Crie sua conta');
+  buttonLabel = computed(() => this.authType() === 'login' ? 'Entrar' : 'Registrar');
+
+
+  handleAuthClick() {
+    if (this.authType() === 'login') {
+      this._login();
     } else {
-      //
+      this._register();
     }
   }
 
-  private _onLoginClick() {
+  private _login() {
     if (this.loginForm.valid) {
       console.log('Dados prontos para o Node.js:', this.loginForm.value);
     } else {
       console.log('Formulário inválido. Verifique os campos.');
-      this.loginForm.markAllAsTouched(); 
+      this.loginForm.markAllAsTouched();
     }
   }
 
-  switchAuthType(){
-    const currentType = this.authType();
-    this.authType.set(currentType === 'login' ? 'register' : 'login');
+  private _register() {
+    if (this.loginForm.valid) {
+      console.log('Dados prontos para o Node.js:', this.loginForm.value);
+    } else {
+      console.log('Formulário inválido. Verifique os campos.');
+      this.loginForm.markAllAsTouched();
+    }
   }
 
+  switchAuthType() {
+    const currentType = this.authType();
+    const currentValues = this.loginForm.value;
+    this.authType.set(currentType === 'login' ? 'register' : 'login');
+    this.loginForm.reset(currentValues);
+  }
 
 }
